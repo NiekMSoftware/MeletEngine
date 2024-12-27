f05083a1b883700a2e163ec6c22678552afe9dd0
Niek Melet <niek+git@meletstudios.com>
2024-12-27 15:25:36 +0100

    Added a simple shader

---
+++ Engine/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shaders/simple/simple_shader.vert -o shaders/simple/simple_shader.vert.spv
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shaders/simple/simple_shader.frag -o shaders/simple/simple_shader.frag.spv
+pause
